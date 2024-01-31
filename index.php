4cf14ab3e7f0026379ebecbea59e52b66c3653df
JIMVANLP <88862034+JIMVANLP@users.noreply.github.com>
2024-01-31 03:59:46 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<title>practica 2 con imagen</title>
+<body>
+<body bgcolor="#696969">
+<center>
+<?php
+echo"<font face = 'Algerian' size  = '150' color= '#800000'>EL JIMMY</font>";
+echo"<font size = '150' color= '#00FF7F'><br>UTT</font>";
+echo"<font size = '150' color= '#0000FF'><br>MATRICULA:20170167</font>";
+?>
+</center>
+</body>
+</html>
